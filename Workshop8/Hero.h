f0af28a577fdc132fb3a5f1e96008dfaa1e16ce6
Alena Mitrakhovich <amitrakhovich@myseneca.ca>
2016-12-18 21:14:44 -0500

    Add files via upload

---
+++ Workshop8/Hero.h
@@ -0,0 +1,32 @@
+// OOP244 Workshop 8: Derived Classes
+// Hero.h
+// Version 1.0
+// Date	2016/11/09
+// Author	Alena Mitrakhovich
+// amitrakhovich@myseneca.ca
+// 115 297 152
+/////////////////////////////////////////////////////
+
+#ifndef ICT_HERO_H__
+#define ICT_HERO_H__
+
+#include <iostream>
+
+class Hero {
+  char name_[21];
+  double strength_;
+public:
+  Hero();
+  Hero(const char* name, double strength);
+  ~Hero();
+  void setEmpty();
+  bool isEmpty() const;
+  double getStrength() const;
+  void display(std::ostream& os) const;
+  void operator-=(double strength);
+  void operator+=(double strength);
+};
+
+bool operator<(const Hero& a, const Hero& b);
+
+#endif
